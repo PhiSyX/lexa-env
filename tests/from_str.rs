7fd01a7a851da8737ae2f5166ff3d9f9c4122c54
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2023-09-07 19:30:55 +0200

    tests: variables d'environnement à partir d'une string

---
+++ tests/from_str.rs
@@ -0,0 +1,32 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃                           __    _            ____  ______                 ┃
+// ┃                    ____  / /_  (_)______  __/ __ \/ ____/                 ┃
+// ┃                   / __ \/ __ \/ / ___/ / / / /_/ / /                      ┃
+// ┃                  / /_/ / / / / (__  ) /_/ / _, _/ /___                    ┃
+// ┃                 / .___/_/ /_/_/____/\__, /_/ |_|\____/                    ┃
+// ┃                /_/                 /____/                                 ┃
+// ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+// ┃ Copyright: (c) 2023, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+#[test]
+fn test_get_env_from_str() {
+	#[derive(serde::Deserialize)]
+	struct MyEnv {
+		key: String,
+		maybe_key: Option<String>,
+	}
+
+	let my_env: MyEnv = lexa_env::from_str(include_str!(".env")).expect(
+		"La structure MyEnv avec les données adéquates du fichier .env",
+	);
+
+	assert_eq!(my_env.key, "VALUE");
+	assert!(my_env.maybe_key.is_none());
+}
